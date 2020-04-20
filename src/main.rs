f9c6ae39ce2fceb7dd33856fb27b4c17e03ef869
Dominik Rosiek <drosiek@sumologic.com>
2020-04-20 15:35:38 +0200

    Add metrics

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,48 @@ async fn handle(
   metrics: Arc<Mutex<u64>>,
   logs: Arc<Mutex<u64>>,
   logs_bytes: Arc<Mutex<u64>>,
+  p_metrics: Arc<Mutex<u64>>,
+  p_logs: Arc<Mutex<u64>>,
+  p_logs_bytes: Arc<Mutex<u64>>,
   ts: Arc<Mutex<u64>>
 ) -> Result<Response<Body>, Infallible> {
     let uri = req.uri().path();
 
     if uri.starts_with("/terraform") {
         Ok(Response::new("{\"source\": {\"url\": \"http://sumock.sumock:3000/receiver\"}}".into()))
+    }
+    else if uri.starts_with("/metrics-json") {
+      let metrics = metrics.lock().unwrap();
+      let logs = logs.lock().unwrap();
+      let logs_bytes = logs_bytes.lock().unwrap();
+      let p_metrics = p_metrics.lock().unwrap();
+      let p_logs = p_logs.lock().unwrap();
+      let p_logs_bytes = p_logs_bytes.lock().unwrap();
+
+      Ok(Response::new(format!("{{
+        \"timestamp\": {},
+        \"last_minute_stats\": {{
+          \"metrics\": {},
+          \"logs\": {},
+          \"logs_bytes\": {}
+        }},
+        \"total_stats\": {{
+          \"metrics\": {},
+          \"logs\": {},
+          \"logs_bytes\": {}
+        }}}}", get_now(), *p_metrics, *p_logs, *p_logs_bytes, *metrics, *logs, *logs_bytes).into()))
+    }
+    else if uri.starts_with("/metrics") {
+      let metrics = metrics.lock().unwrap();
+      let logs = logs.lock().unwrap();
+      let logs_bytes = logs_bytes.lock().unwrap();
+
+      Ok(Response::new(format!("# TYPE sumock_metrics_count counter
+sumock_metrics_count {}
+# TYPE sumock_logs_count counter
+sumock_logs_count {}
+# TYPE sumock_logs_bytes_count counter
+sumock_logs_bytes_count {}", *metrics, *logs, *logs_bytes).into()))
     }
     else {
       let empty_header = HeaderValue::from_str("").unwrap();
@@ -53,25 +89,46 @@ async fn handle(
           println!("invalid header value");
         }
       }
-      stats(metrics, logs, logs_bytes, ts);
+      stats(metrics, logs, logs_bytes, p_metrics, p_logs, p_logs_bytes, ts);
       Ok(Response::new("".into()))
     }
 }
 
-async fn run_app(metrics: Arc<Mutex<u64>>, logs: Arc<Mutex<u64>>, logs_bytes: Arc<Mutex<u64>>, ts: Arc<Mutex<u64>>) {
+async fn run_app(
+  metrics: Arc<Mutex<u64>>,
+  logs: Arc<Mutex<u64>>,
+  logs_bytes: Arc<Mutex<u64>>,
+  p_metrics: Arc<Mutex<u64>>,
+  p_logs: Arc<Mutex<u64>>,
+  p_logs_bytes: Arc<Mutex<u64>>,
+  ts: Arc<Mutex<u64>>) {
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Sumock is waiting for enemy on 0.0.0.0:3000!");
     let make_svc = make_service_fn(|_conn| {
       let metrics = metrics.clone();
       let logs = logs.clone();
       let logs_bytes = logs_bytes.clone();
+      let p_metrics = p_metrics.clone();
+      let p_logs = p_logs.clone();
+      let p_logs_bytes = p_logs_bytes.clone();
       let ts = ts.clone();
       async move {
         let metrics = metrics.clone();
         let logs = logs.clone();
         let logs_bytes = logs_bytes.clone();
+        let p_metrics = p_metrics.clone();
+        let p_logs = p_logs.clone();
+        let p_logs_bytes = p_logs_bytes.clone();
         let ts = ts.clone();
-        let result = service_fn(move |req| handle(req, metrics.clone(), logs.clone(), logs_bytes.clone(), ts.clone()));
+        let result = service_fn(move |req| handle(
+          req,
+          metrics.clone(),
+          logs.clone(),
+          logs_bytes.clone(),
+          p_metrics.clone(),
+          p_logs.clone(),
+          p_logs_bytes.clone(),
+          ts.clone()));
         Ok::<_, Infallible>(result)
     }});
 
@@ -83,18 +140,26 @@ async fn run_app(metrics: Arc<Mutex<u64>>, logs: Arc<Mutex<u64>>, logs_bytes: Ar
 
 }
 
-fn stats(metrics: Arc<Mutex<u64>>, logs: Arc<Mutex<u64>>, logs_bytes: Arc<Mutex<u64>>, ts: Arc<Mutex<u64>>) {
-  let mut metrics = metrics.lock().unwrap();
-  let mut logs = logs.lock().unwrap();
-  let mut logs_bytes = logs_bytes.lock().unwrap();
+fn stats(metrics: Arc<Mutex<u64>>, logs: Arc<Mutex<u64>>, logs_bytes: Arc<Mutex<u64>>, 
+  p_metrics: Arc<Mutex<u64>>, p_logs: Arc<Mutex<u64>>, p_logs_bytes: Arc<Mutex<u64>>, ts: Arc<Mutex<u64>>) {
+  let metrics = metrics.lock().unwrap();
+  let logs = logs.lock().unwrap();
+  let logs_bytes = logs_bytes.lock().unwrap();
+  let mut p_metrics = p_metrics.lock().unwrap();
+  let mut p_logs = p_logs.lock().unwrap();
+  let mut p_logs_bytes = p_logs_bytes.lock().unwrap();
   let mut ts = ts.lock().unwrap();
 
   if get_now() >= *ts + 60 {
-      println!("{} Metrics: {:10.} Logs: {:10.}; {:10} MB/s", *ts, *metrics, *logs_bytes/(get_now()-*ts)/1e6, *logs);
+      println!("{} Metrics: {:10.} Logs: {:10.}; {:6.6} MB/s",
+        *ts,
+        *metrics - *p_metrics,
+        *logs - *p_logs,
+        ((*logs_bytes - *p_logs_bytes) as f64)/((get_now()-*ts) as f64)/(1e6 as f64));
       *ts = get_now();
-      *metrics = 0;
-      *logs = 0;
-      *logs_bytes = 0;
+      *p_metrics = *metrics;
+      *p_logs = *logs;
+      *p_logs_bytes = *logs_bytes;
   }
 }
 
@@ -103,7 +168,10 @@ pub async fn main() {
     let metrics = Arc::new(Mutex::new(0 as u64));
     let logs = Arc::new(Mutex::new(0 as u64));
     let logs_bytes = Arc::new(Mutex::new(0 as u64));
+    let p_metrics = Arc::new(Mutex::new(0 as u64));
+    let p_logs = Arc::new(Mutex::new(0 as u64));
+    let p_logs_bytes = Arc::new(Mutex::new(0 as u64));
     let ts = Arc::new(Mutex::new(get_now()));
 
-    run_app(metrics, logs, logs_bytes, ts).await;
+    run_app(metrics, logs, logs_bytes, p_metrics, p_logs, p_logs_bytes, ts).await;
 }
